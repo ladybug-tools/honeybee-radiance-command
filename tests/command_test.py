86f58d1d81bf706b0cf0d188327a46f712776507
Built Tools <mail@built.tools>
2021-12-01 18:19:04 +0100

    test(Command): Corrected test for paths according to windows and other os.

--- tests/command_test.py
+++ tests/command_test.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 from honeybee_radiance_command._command import Command
 
@@ -34,28 +35,38 @@ def test_subclass():
     assert rtrace.command == 'rtrace'
 
 def test_path_checker():
-    test_path1=r'C:\some directory\some_file.rad'
-    test_path2=r'C:\some directory\some_file.bmp'
+    test_path1=r'test\some directory\some_file.rad'
+    test_path2=r'test\some directory\some_file.bmp'
 
     cmd=Command()
     pth1=cmd.path_checker(test_path1)
-    assert pth1==r'"C:\some directory\some_file.rad"'
+    if os.name=='nt':
+        assert pth1==r'"test\some directory\some_file.rad"'
+    else:
+        assert pth1 == r'"test/some directory/some_file.rad"'
 
     with pytest.raises(ValueError):
         pth2=cmd.path_checker(test_path2,extn_list=['.rad','.sky'])
 
 def test_path_checker_multiple():
-    test_path1=r'C:\some directory\some_file.rad'
-    test_path2=r'C:\some directory\some_file.bmp'
-    test_path3 = r'C:\some directory\some_file2.rad'
+    test_path1=r'test\some directory\some_file.rad'
+    test_path2=r'test\some directory\some_file.bmp'
+    test_path3 = r'test\some directory\some_file2.rad'
 
     cmd=Command()
     pth_list=cmd.path_checker_multiple([test_path1,test_path2])
-    assert pth_list==[r'"C:\some directory\some_file.rad"',
-                  r'"C:\some directory\some_file.bmp"']
+    if os.name=='nt':
+        assert pth_list==[r'"test\some directory\some_file.rad"',
+                  r'"test\some directory\some_file.bmp"']
+    else:
+        assert pth_list == [r'"test/some directory/some_file.rad"',
+                            r'"test/some directory/some_file.bmp"']
 
     pth_list_str=cmd.path_checker_multiple([test_path1,test_path2],outputs_as_string=True)
-    assert pth_list_str==r'"C:\some directory\some_file.rad" "C:\some directory\some_file.bmp"'
+    if os.name=='nt':
+        assert pth_list_str==r'"test\some directory\some_file.rad" "test\some directory\some_file.bmp"'
+    else:
+        assert pth_list_str == r'"test/some directory/some_file.rad" "test/some directory/some_file.bmp"'
 
     with pytest.raises(ValueError):
         pth_list=cmd.path_checker_multiple([test_path1,test_path2,test_path3],
